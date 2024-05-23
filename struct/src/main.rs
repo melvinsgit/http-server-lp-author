3afd2458e260deebcbad4e1167091b4bba75ace5
Melvin Soriano <melvinsoriano@MMBP.local>
2024-05-23 19:49:52 -0400

    4. Complex Data Structures and Types - adding Slovak and Czech languages

--- struct/src/main.rs
+++ struct/src/main.rs
@@ -4,6 +4,8 @@ enum Lang {
   Spanish,
   Chinese,
   Texan,
+  Slovak,
+  Czech
 }
 
 struct Greeting {
@@ -21,6 +23,10 @@ fn main() {
   let g : Greeting = Greeting { lang: Lang::Texan, message: String::from("Howdy WasmEdge!") };
   v.push(g);
   let g : Greeting = Greeting { lang: Lang::Chinese, message: String::from("WasmEdge 你好!") };
+  v.push(g);
+  let g : Greeting = Greeting { lang: Lang::Slovak, message: String::from("Ahoj, WasmEdge!") };
+  v.push(g);
+  let g : Greeting = Greeting { lang: Lang::Czech, message: String::from("Nazdar, WasmEdge!") };
   v.push(g);
 
   for e in v {
